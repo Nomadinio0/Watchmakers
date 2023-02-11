4891f245ac632b23f7b5a23ad2a4ffa74ca35815
Artur <a.wolniak01@gmail.com>
2023-02-11 23:33:03 +0100

    added grid gallery and img zooming js fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,7 @@
 const footerYear = document.querySelector('.footer__year')
-const galleryBig = document.querySelectorAll('gallery-img')
-
-const zoomImg = () => {
-	
-}
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
-
-galleryBig.addEventListener('click', zoomImg)
 handleCurrentYear()
