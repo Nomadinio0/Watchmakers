16224236d06edbb8295f3ceb270b2e22f58af659
Artur <a.wolniak01@gmail.com>
2023-03-30 01:02:08 +0200

    version v.1.0

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
 const footerYear = document.querySelector('.footer__year')
+const mobileNav = document.querySelector('.navbar__items')
+const burgerTransform = document.querySelector('.burgerBtn')
+
+burgerTransform.addEventListener('click', function(){
+	mobileNav.classList.toggle('navbar__items-active');
+})
 
 const handleCurrentYear = () => {
 	const year = new Date().getFullYear()
 	footerYear.innerText = year
 }
+
 handleCurrentYear()
